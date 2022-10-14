ff619ae3c89137f9f3b2e328580f493d26b7705a
Toba Shaukat <toba.thetasolutions@gmail.com>
2022-10-14 17:52:04 +0500

    Add project files.

---
+++ Net6APIWithJWT/Models/UserConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Net6APIWithJWT.Models
+{
+    // We are not taking data from data base so we get data from constant
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new()
+            {
+                    new UserModel(){ Username="theta",Password="theta",Role="Admin"}
+            };
+    }
+}
---
+++ Net6APIWithJWT/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Net6APIWithJWT.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
